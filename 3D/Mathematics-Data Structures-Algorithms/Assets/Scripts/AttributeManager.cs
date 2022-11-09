0b567df8315f6f34be16f0f0529e27748295fe2e
Roman Vitolo <68485569+RomanVitolo@users.noreply.github.com>
2022-11-08 23:12:19 -0300

    Import Packages

---
+++ 3D/Mathematics-Data Structures-Algorithms/Assets/Scripts/AttributeManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AttributeManager : MonoBehaviour
+{
+    public Text attributeDisplay;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 screenPoint = Camera.main.WorldToScreenPoint(this.transform.position);
+        attributeDisplay.transform.position = screenPoint + new Vector3(0,-50,0);
+    }
+       
+}
